be76ff85c4ad51e76f2e75c7b3b634f7e041e1ec
Christopher Fofrich <cfofrich@gmail.com>
2020-11-10 19:28:26 -0500

    Add project files.

---
+++ DiceGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice simulator. Please enter the amount of dice you would like to cast (Press Q to quit) >>");
+
+            string diceString = "";
+
+            while (diceString != "Q")
+            {
+                diceString = Console.ReadLine();
+
+                if (diceString == "Q")
+                {
+                    break;
+                }
+
+                int diceInput = Convert.ToInt32(diceString);
+
+                for (int index = 0; index < diceInput; index++)
+                {
+                    Random random = new Random();
+                    int diceRoll = random.Next(6) + 1;
+
+                    Console.WriteLine(diceRoll);
+
+                }
+                Console.WriteLine("Enter another amount of dice to case or 'Q' to quit >>");
+            }
+
+            
+
+        }
+
+    }
+}
